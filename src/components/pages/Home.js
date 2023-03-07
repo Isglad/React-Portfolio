8a51740d44e0dacd22d52939382f7a85ccbd6760
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-03-06 23:56:13 -0500

    add home page content

---
+++ src/components/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hi, my name is</p>
+      <h1>Gladys Ange Isingizwe.<br/>
+      I build things for the web.</h1>
+      <p>
+        Passionate and solutions-driven full stack web developer with a web
+        development certificate from UC Berkeley Extension. Experience in responsive
+        design utilising a combination of strong communication, collaboration,
+        problem-solving, and analytical skills. Strong ability to design, build,
+        and maintain websites from conception to production. Ability to work in
+        a fast-paced and diverse team environment developing solutions and
+        exceeding expectations.
+      </p>
+    </div>
+  );
+}
