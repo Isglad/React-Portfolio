8b6e50cc31d59f4de426328eb794952f0104cb6e
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-03-06 23:58:02 -0500

    add social links icon

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="footer bg-dark fixed-bottom">
+            <a href="https://github.com/Isglad" target="_blank" rel="noreferrer">
+            <img
+                src="https://img.icons8.com/color/48/000000/github--v1.png"
+                alt="GitHub"
+                className="icon"
+            />
+            </a>
+            <a href="https://www.linkedin.com/in/gladys-isingizwe/" target="_blank" rel="noreferrer">
+            <img
+                src="https://img.icons8.com/color/48/000000/linkedin.png"
+                alt="LinkedIn"
+                className="icon"
+            />
+            </a>
+            <a href="https://medium.com/@gladyisingizwe" target="_blank" rel="noreferrer">
+            <img
+                src="https://img.icons8.com/color/48/000000/medium-monogram.png"
+                alt="Medium"
+                className="icon"
+            />
+            </a>
+        </footer>
+    );
+}
