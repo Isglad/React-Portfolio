40be996cfd5268a44225d1f02faa0064eddefaf7
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-03-08 23:49:48 -0500

    replaced div by section

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,20 +1,29 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hi, my name is</p>
-      <h1>Gladys Ange Isingizwe.<br/>
-      I build things for the web.</h1>
-      <p>
-        Passionate and solutions-driven full stack web developer with a web
-        development certificate from UC Berkeley Extension. Experience in responsive
-        design utilising a combination of strong communication, collaboration,
-        problem-solving, and analytical skills. Strong ability to design, build,
-        and maintain websites from conception to production. Ability to work in
-        a fast-paced and diverse team environment developing solutions and
-        exceeding expectations.
-      </p>
-    </div>
+    <section className="hero">
+      <div className="content">
+        <h1>Hi, my name is</h1>
+      </div>
+      <div className="myName">
+        <h2 className="big-heading">Gladys Ange Isingizwe.</h2>
+      </div>
+      <div className="text">
+        <h3 className="big-heading">I build things for the web.</h3>
+      </div>
+      <div className="text-content">
+        <p>
+          I'm passionate and solutions-driven full stack web developer with a
+          web development certificate from UC Berkeley Extension. Experienced in
+          responsive design utilizing a combination of strong communication,
+          collaboration, problem-solving, and analytical skills. Strong ability
+          to design, build, and maintain websites from conception to production.
+          Ability to work in a fast-paced and diverse team environment
+          developing solutions and exceeding expectations.
+        </p>
+      </div>
+    </section>
   );
 }
