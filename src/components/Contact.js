32e52fbdc2b0f1a360d7d1a13de859086c517153
Gladys Ange Isingizwe <gladyisingizwe@gmail.com>
2023-03-08 23:53:27 -0500

    add more div elements to get a better styled page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Contact() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const [isNameValid, setIsNameValid] = useState(true);
   const [isEmailValid, setIsEmailValid] = useState(true);
   const [isMessageValid, setIsMessageValid] = useState(true);
@@ -52,65 +52,77 @@ function Contact() {
   };
 
   return (
-    <section id="contact">
-      <h2>Contact</h2>
-      {!isFormSubmitted && (
-        <form onSubmit={handleSubmit}>
-          <div>
-            <div>
-              <label htmlFor="name">Name</label>
-            </div>
-            <div>
-              <input
-                id="name"
-                type="text"
-                value={name}
-                onChange={handleNameChange}
-                className={!isNameValid ? 'invalid' : ''}
-              />
-            </div>
-            
-            {!isNameValid && <p className="error">Please enter your name.</p>}
+    <section className="container" id="contact">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-6 col-md-8 col-sm-10">
+            <h2>Contact</h2>
+            {!isFormSubmitted && (
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <div>
+                    <label htmlFor="name">Name</label>
+                  </div>
+                  <div>
+                    <input
+                      id="name"
+                      type="text"
+                      value={name}
+                      onChange={handleNameChange}
+                      className={!isNameValid ? "invalid" : ""}
+                    />
+                  </div>
+
+                  {!isNameValid && (
+                    <p className="error">Please enter your name.</p>
+                  )}
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor="email">Email</label>
+                  </div>
+                  <div>
+                    <input
+                      id="email"
+                      type="email"
+                      value={email}
+                      onChange={handleEmailChange}
+                      className={!isEmailValid ? "invalid" : ""}
+                    />
+                  </div>
+
+                  {!isEmailValid && (
+                    <p className="error">Please enter a valid email address.</p>
+                  )}
+                </div>
+                <div>
+                  <div>
+                    <label htmlFor="message">Message</label>
+                  </div>
+                  <div>
+                    <textarea
+                      id="message"
+                      value={message}
+                      onChange={handleMessageChange}
+                      className={!isMessageValid ? "invalid" : ""}
+                    ></textarea>
+                  </div>
+
+                  {!isMessageValid && (
+                    <p className="error">Please enter a message.</p>
+                  )}
+                </div>
+                <button type="submit">Send</button>
+              </form>
+            )}
+            {isFormSubmitted && (
+              <div>
+                <p>Thank you for your message, {name}!</p>
+              </div>
+            )}
           </div>
-          <div>
-            <div>
-              <label htmlFor="email">Email</label>
-            </div>
-            <div>
-              <input
-                id="email"
-                type="email"
-                value={email}
-                onChange={handleEmailChange}
-                className={!isEmailValid ? 'invalid' : ''}
-              />
-            </div>
-            
-            {!isEmailValid && <p className="error">Please enter a valid email address.</p>}
-          </div>
-          <div>
-            <div>
-              <label htmlFor="message">Message</label>
-            </div>
-            <div>
-              <textarea
-                id="message"
-                value={message}
-                onChange={handleMessageChange}
-                className={!isMessageValid ? 'invalid' : ''}
-              ></textarea>
-            </div>
-            
-            {!isMessageValid && <p className="error">Please enter a message.</p>}
-          </div>
-          <button type="submit">Send</button>
-        </form>
-      )}
-      {isFormSubmitted && (
-        <div>
-          <p>Thank you for your message, {name}!</p>
         </div>
-      )}
+      </div>
     </section>
   );
 }
